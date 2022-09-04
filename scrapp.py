7a4d0dacea22d7bcdef0e3fbae8677d80656b2b2
EdwinDij <edwin.d899@gmail.com>
2022-09-04 12:59:42 +0200

    extraction faite

--- scrapp.py
+++ scrapp.py
@@ -5,7 +5,9 @@ page = requests.get(url)
 #voir le code html source
 #parsing
 soup = BeautifulSoup(page.content, 'html.parser')
-titles = soup.find_all("a", class_="gem-c-document-list__item-title")
+titles_bs = soup.find_all("a", class_="gem-c-document-list__item-title")
 #print(titles)
-for title in titles:
-    print(title.string)
+titles = []
+for title in titles_bs:
+    titles.append(title.string)
+    print(titles)
